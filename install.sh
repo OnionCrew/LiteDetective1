9031ff8ad611de5ed24e2d1b7272fd6f02cf3db2
OnionCrew <61432801+OnionCrew@users.noreply.github.com>
2020-04-29 22:00:04 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+cp webhack /data/data/com.termux/files/usr/bin/
+cd /data/data/com.termux/files/usr/bin/
+chmod +777 webhack
+cd $Home
+pip3 install requests
+pip3 install pyfiglet
+pip3 install tqdm
+pip3 install termcolor
+clear
